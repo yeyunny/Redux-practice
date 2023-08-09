8e71a7b75a4f6d552f3fe05a82be7437beb420bb
yeyun <hallo.yunye@gmail.com>
2023-08-09 18:54:44 +0900

    [ADD] redux에 state 추가

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,6 +11,7 @@ const Counter = () => {
   // state를 넣고 state.counter라는 이 컴포넌트에서 필요로 하는 상태 부분 받음
   // store의 상태 값이 변경될 때마다 자동으로 업데이트 되고 최신카운터 받음!!!
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -24,12 +25,14 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment by 5</button>
