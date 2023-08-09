e3c51b1040d7e68bd83898201e4f93844beaaa9e
yeyun <hallo.yunye@gmail.com>
2023-08-09 22:27:51 +0900

    [ADD] redux를 사용하여 log-in,out 구현해보기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Fragment } from "react";
+import { useSelector } from "react-redux";
+
 import Header from "./components/Header";
 import Auth from "./components/Auth";
 import Counter from "./components/Counter";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  //최상단에서 auth 상태 보내기
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {/* auth임을 검증하여 보여줄 페이지를 정함 */}
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
