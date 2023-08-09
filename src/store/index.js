22ffdcc95c2a1216b2ead1fb067a89f13a72face
yeyun <hallo.yunye@gmail.com>
2023-08-09 20:26:05 +0900

    [ADD] reduxjs/toolkit 사용해보기

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+// 기존 state 값을 변경하는 것처럼 보이지만, 사실 그렇지 않음!
+// 기존 state를 복제하여 새로운 상태 객체를 생성하여 이용
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
